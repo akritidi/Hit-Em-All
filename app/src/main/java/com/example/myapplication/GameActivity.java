6be0118e00ce97a229511d94047212b348775814
danai <iegatsou@csd.auth.gr>
2020-04-30 23:21:22 +0300

    Game Activity

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -5,9 +5,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.ImageDecoder;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,7 +20,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
-
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,7 +32,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private Toast hitToast;
     private Toast missToast;
     private int lives;
-    private Random randMole;
+    private int r;
     private ImageButton[] arrayOfButtons;
     private ImageButton imageButton1;
     private ImageButton imageButton2;
@@ -39,18 +43,24 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton imageButton7;
     private ImageButton imageButton8;
     private ImageButton imageButton9;
+    private int mInterval;
+    private Handler mHandler;
+
+
 
     @SuppressLint({"ClickableViewAccessibility", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        createButtons();
 
         countdownFinished = false;
         lives=3;
         countdownSound = MediaPlayer.create(this, R.raw.countdown_sound);
         countdownText = findViewById(R.id.textCountdown);
         Timer countdown = new Timer();
+        final Timer moles=new Timer();
         countdownSound.start();
         countdown.schedule(new TimerTask() {
             @Override
@@ -59,7 +69,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             }
         },1000);
 
-        createButtons();
+//        mInterval=5000; //Initializing the "delay" time period
+         startRepeatingTask();
 
         hitSound = MediaPlayer.create(this,R.raw.hit);
         missSound = MediaPlayer.create(this,R.raw.miss);
@@ -81,6 +92,41 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         });
 
     }
+
+    public void startRepeatingTask() {
+        Handler mHandler=new Handler();
+
+
+        r = randomMole();
+        mHandler.postDelayed(runnableCode, 3000);
+
+    }
+
+    public void hideMole(int r){
+        Handler mHandler2=new Handler();
+        mHandler2.postDelayed(runnableCode2,1500);
+//        startRepeatingTask();
+    }
+
+    private Runnable runnableCode=new Runnable() {
+        @Override
+        public void run() {
+            Log.d("Handlers","Called on Main Thread");
+            arrayOfButtons[r].setVisibility(View.VISIBLE);
+            hideMole(r);
+        }
+    };
+
+    private Runnable runnableCode2= new Runnable() {
+        @Override
+        public void run() {
+            Log.d("Handlers","Called on Main Thread");
+            arrayOfButtons[r].setVisibility(View.GONE);
+            startRepeatingTask();
+        }
+    };
+
+
     public void updateCountdown(){
         countdownText.setText("2");
         try {
@@ -96,7 +142,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             e.printStackTrace();
         }
         countdownFinished=true;
-        randomMole();
+
+
     }
     @SuppressLint("SetTextI18n")
     public void missToast(){
@@ -139,30 +186,20 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
     public int randomMole(){
-        while(lives>=1){
-            randMole = new Random();
-            int iMole;
-            try {
-                Thread.sleep(2000);
-                iMole = randMole.nextInt(0-8);
-                enableButton(iMole);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        int iMole;
+        iMole = new Random().ints(0,8).limit(1).findFirst().getAsInt();
+   return iMole; }
 
-        }
-        return -1;
-    }
     public void createButtons(){
-        imageButton1 = findViewById(R.id.imageButton1);
-        imageButton2 = findViewById(R.id.imageButton2);
-        imageButton3 = findViewById(R.id.imageButton3);
-        imageButton4 = findViewById(R.id.imageButton4);
-        imageButton5 = findViewById(R.id.imageButton5);
-        imageButton6 = findViewById(R.id.imageButton6);
-        imageButton7 = findViewById(R.id.imageButton7);
-        imageButton8 = findViewById(R.id.imageButton8);
-        imageButton9 = findViewById(R.id.imageButton9);
+        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imageButton7 = (ImageButton)findViewById(R.id.imageButton7);
+        imageButton8 = (ImageButton)findViewById(R.id.imageButton8);
+        imageButton9 = (ImageButton)findViewById(R.id.imageButton9);
 
         imageButton1.setOnClickListener(this);
         imageButton2.setOnClickListener(this);
@@ -186,7 +223,5 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         arrayOfButtons[8] = imageButton9;
     }
 
-    public void enableButton(int i){
-        arrayOfButtons[i].setVisibility(ImageButton.VISIBLE);
-    }
+
 }
